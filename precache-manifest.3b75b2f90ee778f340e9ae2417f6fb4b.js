0f3431541027ca47c408fe31fdd1d7ee6c9773f1
mrchrischan <chanchris1117@gmail.com>
2020-02-12 13:36:27 -0500

    Updates

---
+++ precache-manifest.3b75b2f90ee778f340e9ae2417f6fb4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf731fff7953842d6d396010f658da8",
+    "url": "/little-rocket-test/index.html"
+  },
+  {
+    "revision": "7d0f47a36a7250e4614d",
+    "url": "/little-rocket-test/static/css/main.7afcafef.chunk.css"
+  },
+  {
+    "revision": "b37c547e32dc3e96c6c0",
+    "url": "/little-rocket-test/static/js/2.36766a9f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/little-rocket-test/static/js/2.36766a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0f47a36a7250e4614d",
+    "url": "/little-rocket-test/static/js/main.5f14a57a.chunk.js"
+  },
+  {
+    "revision": "a67f27d9dffdcb401363",
+    "url": "/little-rocket-test/static/js/runtime-main.6575394e.js"
+  }
+]);
